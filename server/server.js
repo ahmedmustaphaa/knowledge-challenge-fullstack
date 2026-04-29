58470b7e2acdacf051d5aa7dfc37435150173c43
Ahmed Mustafa <ahmed@example.com>
2026-04-29 11:05:39 +0300

    dfds

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  credentials: true
+}));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Connected to MongoDB (ES Modules Mode)'))
   .catch((err) => console.error('❌ MongoDB Connection Error:', err));
